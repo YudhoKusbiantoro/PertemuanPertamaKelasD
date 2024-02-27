b06df80e2fde7d842fa6d9e9d7c6bfc8d3a7bfb7
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2024-02-27 11:16:12 +0700

    Add project files.

---
+++ PertemuanPertamaKelasD/PertemuanPertamaKelasD.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+
+	int nAlas, nTinggi, nluas;
+	cout << "Masukan Alasnya= ";
+	cin >> nAlas;
+	cout << "Masukan Tingginya= ";
+	cin >> nTinggi;
+	nluas = 0, 5 * (nAlas * nTinggi);
+	cout << "Luasnya Adalah= " << nluas << endl;
+
+	system("pause");
+}//end
